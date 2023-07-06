98a833fdb16f9dfcbdf9c582d1635f036fdeffb5
Diego Alexandre <ddsa.1988@outlook.com>
2023-07-06 07:03:17 -0300

    commit

--- 005 - Intermediate Javascript/002 - Leap Year/main.js
+++ 005 - Intermediate Javascript/002 - Leap Year/main.js
@@ -6,4 +6,22 @@ function isLeapYear(year) {
     if (!isDataValid) {
         return false;
     }
+
+    const leap =
+        year % 4 === 0
+            ? year % 100 !== 0
+                ? true
+                : year % 400 === 0
+                ? true
+                : false
+            : false;
+
+    return leap;
 }
+
+console.log(isLeapYear(2023));
+console.log(isLeapYear(2020));
+console.log(isLeapYear(2000));
+console.log(isLeapYear(2010));
+console.log(isLeapYear(1999));
+console.log(isLeapYear(1988));
